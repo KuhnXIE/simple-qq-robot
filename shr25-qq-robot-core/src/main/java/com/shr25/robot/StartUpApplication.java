4d7f6706d92bf3eca036fbbdd28d6d46e38a88dc
huobing <dqfe123@163.com>
2023-03-12 20:53:10 +0800

    删除协议相关代码

--- shr25-qq-robot-core/src/main/java/com/shr25/robot/StartUpApplication.java
+++ shr25-qq-robot-core/src/main/java/com/shr25/robot/StartUpApplication.java
@@ -1,11 +1,7 @@
 package com.shr25.robot;
 
-import com.shr25.robot.protocol.FixProtocolVersion;
-import net.mamoe.mirai.utils.BotConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Map;
 
 /**
  * 启动类
@@ -16,9 +12,7 @@ import java.util.Map;
 public class StartUpApplication {
 
     public static void main(String[] args) throws Exception {
-        FixProtocolVersion.update();
-        Map<BotConfiguration.MiraiProtocol, String> miraiProtocol = FixProtocolVersion.info();
-        SpringApplication.run(StartUpApplication.class, args);
+       SpringApplication.run(StartUpApplication.class, args);
         Thread.currentThread().join();
     }
 }
